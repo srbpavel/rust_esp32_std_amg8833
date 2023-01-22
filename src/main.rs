8525cb938511e219e9237d3b0556a128891db374
prace@srbpavel.cz <prace@srbpavel.cz>
2023-01-22 18:25:44 +0100

    status

--- src/main.rs
+++ src/main.rs
@@ -51,16 +51,16 @@ fn main() -> Result<(), WrapError<I2cError>> {
 
     let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
 
-    // /* // VALID
+    // VALID
     let pin_scl = peripherals.pins.gpio8;
     let pin_sda = peripherals.pins.gpio10;
-    // */
+
     /* // INVALID -> pin's swapped
-    // does not panic, but we do not know why it has stopped !!!
     let pin_scl = peripherals.pins.gpio10; // 8
     let pin_sda = peripherals.pins.gpio8; // 10
     */
 
+    /// just to see type for error imlp
     let i2c: Result<esp_idf_hal::i2c::I2cDriver<'_>, esp_idf_sys::EspError> =
         i2c::config(peripherals.i2c0, pin_sda, pin_scl);
 
@@ -74,16 +74,25 @@ fn main() -> Result<(), WrapError<I2cError>> {
             let raw_temp: Result<u16, grideye::Error<_>> = grideye.get_device_temperature_raw();
 
             warn!(
-                "[{}] device >>> raw: {:0>16} / temperature: {}°C",
-                cycle_counter, // since last flash or boot
+                "[{}] device >>> raw: {:0>16} / temperature: {}°C / status: {}",
+                cycle_counter, // since last flash or soft-boot
                 format!("{:b}", raw_temp?),
                 grideye.get_device_temperature_celsius()?,
+                grideye.pixel_temperature_out_ok()?,
             );
 
             if let Ok(framerate) = grideye.get_framerate() {
                 warn!("framerate: Fps{}", FramerateWrap(framerate));
             }
 
+            /// 64 63 62 61 60 59 58 57
+            /// 56 55 54 53 52 51 50 49
+            /// 48 47 46 45 44 43 42 41
+            /// 40 39 38 37 36 35 34 33
+            /// 32 31 30 29 28 27 26 25
+            /// 24 23 22 21 20 19 18 17
+            /// 16 15 14 13 12 11 10  9
+            ///  8  7  6  5  4  3  2  1
             let mut heat_map = sensor_agm::HeatMap([[0.0; 8]; 8]);
 
             info!("array occupies {} bytes", std::mem::size_of_val(&heat_map));
@@ -92,12 +101,17 @@ fn main() -> Result<(), WrapError<I2cError>> {
                 (0..8_u8).into_iter().for_each(|y| {
                     let pixel = (x * 8) + y;
 
-                    /// we don't want to fall only beauce a single pixel error
+                    ///status
+                    if x.eq(&0) && y.eq(&0) {
+                        warn!("status: {:?}", grideye.pixel_temperature_out_ok());
+                    }
+                    
+                    /// we don't want to fall only beause a single pixel error
                     let temp = match grideye.get_pixel_temperature_celsius(pixel) {
                         Ok(pixel_temp) => pixel_temp,
                         Err(e) => {
                             /// just to see
-                            error!("Error reaging pixel temperature: {e:?}");
+                            error!("Error reading pixel x: {x} y: {y} temperature: {e:?}");
 
                             TEMPERATURE_ERROR_VALUE
                         }
