d9ea315d23d6ab2036686cd985bd134f8f5cf349
prace@srbpavel.cz <prace@srbpavel.cz>
2023-01-22 16:03:28 +0100

    let's verify error's

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use errors::WrapError;
 use sensor_agm::FramerateWrap;
 
 use esp_idf_sys as _;
+use esp_idf_sys::EspError;
 
 use esp_idf_svc::log::EspLogger;
 use esp_idf_svc::systime::EspSystemTime;
@@ -15,6 +16,7 @@ use embedded_hal::blocking::delay::DelayMs;
 
 use esp_idf_hal::delay::Ets;
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::i2c::I2cError;
 
 use grideye::Address;
 use grideye::GridEye;
@@ -32,15 +34,8 @@ use log::warn;
 
 const SLEEP_DURATION: u16 = 30 * 1000;
 
-#[allow(unused_imports)]
-use esp_idf_hal::i2c::I2cError;
-#[allow(unused_imports)]
-use esp_idf_sys::EspError;
-
-//fn main() -> anyhow::Result<()> {
-//fn main() -> anyhow::Result<(), WrapError> {
-//fn main() -> Result<(), WrapError> {
-fn main() -> Result<(), WrapError<esp_idf_hal::i2c::I2cError>> {
+//
+fn main() -> Result<(), WrapError<I2cError>> {
     esp_idf_sys::link_patches();
     EspLogger::initialize_default();
 
@@ -58,7 +53,7 @@ fn main() -> Result<(), WrapError<esp_idf_hal::i2c::I2cError>> {
     let pin_sda = peripherals.pins.gpio10;
 
     let i2c: Result<esp_idf_hal::i2c::I2cDriver<'_>, EspError> =
-        i2c::config(peripherals.i2c0, pin_sda, pin_scl);//?;
+        i2c::config(peripherals.i2c0, pin_sda, pin_scl);
 
     let mut grideye = GridEye::new(i2c?, delay, Address::Standard);
 
@@ -66,13 +61,6 @@ fn main() -> Result<(), WrapError<esp_idf_hal::i2c::I2cError>> {
         loop {
             cycle_counter += 1;
 
-            // grideye::Error
-            //
-            // #[derive(Debug)]
-            // pub enum Error<E> {
-            //    /// I2C bus error
-            //    I2c(E),
-            // }
             let raw_temp: Result<u16, grideye::Error<_>> =
                 grideye.get_device_temperature_raw();
 
